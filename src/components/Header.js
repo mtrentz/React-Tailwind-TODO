1e2270b4b3e3c2e1067287617c8689df58c7c2e1
mateus trentz <mateus.trentz@gmail.com>
2021-10-24 21:22:19 -0300

    simple todo, not functional

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className="bg-gray-700 text-gray-50 text-lg font-semibold h-12 text-center pt-2">
+            My TO-DO List
+        </div>
+    )
+}
+
+export default Header
